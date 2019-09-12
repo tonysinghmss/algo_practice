84bcaa9c1075560b30abcb8f1df3ce46edddb0b3
tonysinghmss <manishankarsngh007@gmail.com>
2019-09-12 22:29:00 +0530

    min coins required

--- machete/practice/dp_problems.py
+++ machete/practice/dp_problems.py
@@ -1,4 +1,24 @@
-# Given an array of denominations of coins "nums" and target amount N, calculate
-# minimum possible number of coins used.
+from typing import List
+from collections import defaultdict
+import numpy as np
 
-def 
+def dp_min_coins_count(denoms:List[int], N:int) -> int:
+    """Given an array of denominations of coins "denoms" and target amount "N", calculate
+    minimum possible number of coins used."""
+    # [1,2,5] 
+    # 11
+    #base cases
+    dp = np.full((N+1,), np.inf)
+    # print(np.isposinf(dp))
+    dp[0] = 0
+    for i in range(1,N+1):
+        for x in denoms:
+            # print('{} {}'.format(dp[i], dp[i-x]))
+            dp[i] = min(dp[i], dp[i-x]+1)
+            # print('{} : {}'.format(i, dp[i]))
+    print(dp[N])
+
+
+
+if __name__ == "__main__":
+    dp_min_coins_count([1,2,5], 11)    
